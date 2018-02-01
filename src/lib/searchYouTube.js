94a3cd43eb36b69742b99e4df64325a7894ef60e
tremarkley <tremarkley@gmail.com>
2018-01-31 21:29:24 -0800

    Fill out the searchYouTube function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,19 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = ({query, max, key}, callback) => {
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      q: query, 
+      maxResults: max, 
+      key: key,
+      type: 'video',
+      part: 'snippet',
+      videoEmbeddable: true
+    }
+  }).done(({items}) => {
+      // console.log(`Successful get: ${JSON.stringify(items)}`);
+      callback(items);
+  })
 };
 
 window.searchYouTube = searchYouTube;
